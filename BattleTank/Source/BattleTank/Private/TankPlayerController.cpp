d6aed2b714568a651983b224691899815682221c
R3dWulf <theakaiokami@gmail.com>
2017-11-11 13:55:20 -0500

    '

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,20 +4,26 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
-void ATankPlayerController::BeginPlay() 
+void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
 	auto ControlledTank = GetControlledTank();
-	if(!ControlledTank)
+	if (!ControlledTank)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
 	}
-	else 
+	else
 	{
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possing: %s"), *ControlledTank->GetName());
 	}
-	
+
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const 
@@ -25,3 +31,11 @@ ATank* ATankPlayerController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get world location if linetrace through crosshair
+	// If it hits the landscape 
+		// Tell controlled tank to aim at this point
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,9 +14,16 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
 public:
 	ATank* GetControlledTank() const;
-	
+
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+private: 
+	// Start the tank moving the barrel so that a shot would hit the crosshair
+	void AimTowardsCrosshair();
+		
 };
