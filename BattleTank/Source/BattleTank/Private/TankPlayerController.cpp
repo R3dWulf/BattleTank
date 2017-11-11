eec9961c4b1c4c76c8a48e48c0182c6dc866a05c
R3dWulf <theakaiokami@gmail.com>
2017-11-10 20:54:40 -0500

    '

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+#include "BattleTank.h"
+
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if(!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possing: %s"), *ControlledTank->GetName());
+	}
+	
+}
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+}
+
---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" //.generated.h files must be the latest include or Unreal complains
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const;
+	
+	virtual void BeginPlay() override;
+};
